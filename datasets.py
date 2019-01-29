29eec63f070b54f72acd1f310d9bb00b38d19259
proceduralia <pierluca.doro@gmail.com>
2019-01-29 14:17:01 +0100

    improved training script and dataset. still evaluation and tensorboard missing

--- datasets.py
+++ datasets.py
@@ -12,31 +12,48 @@ class TransformedCifarDataset(Dataset):
     """
      Dataset with (x, transformed_x) couples, given CIFAR10 and a skimage-style transformation
     """
-    def __init__(self, transformation, root='./data', train=True, normalize=True):
-        """Args:
-            transformation (callable): the skimage-style transformation to be applied
-            root (str): the root of the original cifar data
-            train (bool): true for training set, false for test set
-            normalize (bool): if true, normalize the data in [-1,1]
-        """
-        if train:
-            data = torchvision.datasets.CIFAR10(root='./data', train=True, download=True).train_data
-            gray_data = np.array([rgb2gray(im) for im in data])
-            self.original_data = torch.FloatTensor(gray_data)
-            self.transformed_data = torch.FloatTensor(np.array([transformation(im) for im in gray_data]))
+    def __init__(self, transformation, root='./data', tag='adhist', train=True, normalize=True):
+      """Args:
+          transformation (callable): the skimage-style transformation to be applied
+          root (str): the root of the original cifar data
+          train (bool): true for training set, false for test set
+          normalize (bool): if true, normalize the data in [-1,1]
+      """
+      original_train_path = os.path.join(root, "original_train"+tag+'.pt')
+      transformed_train_path = os.path.join(root, "transformed_train"+tag+'.pt')
+      original_test_path = os.path.join(root, "original_test"+tag+'.pt')
+      transformed_test_path = os.path.join(root, "transformed_test"+tag+'.pt')
+      
+      if train:
+        if os.path.exists(original_train_path):
+          self.original_data = torch.load(original_train_path)
+          self.transformed_data = torch.load(transformed_train_path)
         else:
-            data = torchvision.datasets.CIFAR10(root='./data', train=False, download=True).test_data
-            gray_data = np.array([rgb2gray(im) for im in data])
-            self.original_data = torch.FloatTensor(gray_data)
-            self.transformed_data = torch.FloatTensor(np.array([transformation(im) for im in gray_data]))
-        
+          data = torchvision.datasets.CIFAR10(root='./data', train=True, download=True).train_data
+          gray_data = np.array([rgb2gray(im) for im in data])
+          self.original_data = torch.FloatTensor(gray_data)
+          self.transformed_data = torch.FloatTensor(np.array([transformation(im) for im in gray_data]))
+          torch.save(self.original_data, original_train_path)
+          torch.save(self.transformed_data, transformed_train_path)
+
+      if not train:
+        if os.path.exists(original_test_path):
+          self.original_data = torch.load(original_test_path)
+          self.transformed_data = torch.load(transformed_test_path)
+        else:
+          data = torchvision.datasets.CIFAR10(root='./data', train=False, download=True).test_data
+          gray_data = np.array([rgb2gray(im) for im in data])
+          self.original_data = torch.FloatTensor(gray_data)
+          self.transformed_data = torch.FloatTensor(np.array([transformation(im) for im in gray_data]))
+          torch.save(self.original_data, original_test_path)
+          torch.save(self.transformed_data, transformed_test_path)
+   
         if normalize:
             normalization = transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
             self.original_data = normalization(self.original_data)
             self.transformed_data = normalization(self.transformed_data)
-
-        self.original_data = self.original_data.unsqueeze(1)
-        self.transformed_data = self.transformed_data.unsqueeze(1)
+      self.original_data = self.original_data.unsqueeze(1)
+      self.transformed_data = self.transformed_data.unsqueeze(1)
 
     def __getitem__(self, i):
         return self.original_data[i], self.transformed_data[i]
--- learn_transform.py
+++ learn_transform.py
@@ -9,7 +9,7 @@ from datasets import TransformedCifarDataset
 from skimage.exposure import equalize_hist, equalize_adapthist
 from transforms import unsharp_mask
 from scipy.stats import wasserstein_distance
-from models import MLP
+from models import MLP, NaiveCNN, LittleUnet
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--batch_size', type=int, default=8, help='input batch size')
@@ -21,9 +21,15 @@ parser.add_argument('--manualSeed', type=int, help='manual seed')
 parser.add_argument('--logdir', default='log', help='logdir for tensorboard')
 parser.add_argument('--run_tag', default='', help='tags for the current run')
 parser.add_argument('--checkpoint_every', default=10, help='number of epochs after which saving checkpoints') 
-parser.add_argument('--model_type', default='mlp', help='type of model to use')
-parser.add_argument('--transform', default='hist_eq', choices=['hist_eq','ad_hist_eq','unsharp'], help='transformation to be learned')
+parser.add_argument('--model_type', default='unet', choices=['unet', 'cnn', 'mlp'], help='type of model to use')
+parser.add_argument('--transform', default='ad_hist_eq', choices=['hist_eq','ad_hist_eq','unsharp'], help='transformation to be learned')
 opt = parser.parse_args()
+print(opt)
+
+if torch.cuda.is_available() and not opt.cuda:
+	print("You should run with CUDA.")
+device = torch.device("cuda:1" if opt.cuda else "cpu")
+print("Using", torch.cuda.get_device_name(device))
 
 transforms = {
     'hist_eq': equalize_hist,
@@ -35,14 +41,23 @@ dataset = TransformedCifarDataset(transforms[opt.transform])
 loader = DataLoader(dataset, batch_size=opt.batch_size,
             shuffle=True, num_workers=2)
 
-model = MLP()
+if opt.model_type == 'unet':
+  model = LittleUnet()
+if opt.model_type == 'cnn':
+  model = NaiveCNN()
+if opt.model_type == 'mlp':
+  model = MLP()
+assert model
+
+model = model.to(device)
 model.train()
-criterion = nn.MSELoss()
-optimizer = optim.SGD(model.parameters(), lr=opt.lr, momentum=0.9)
+criterion = nn.MSELoss().to(device)
+optimizer = optim.Adam(model.parameters(), lr=opt.lr)
 
 for epoch in range(opt.epochs):
     running_loss = 0.0
     for i, (im_o, im_t) in enumerate(loader):
+        im_o, im_t = im_o.to(device), im_t.to(device)
         optimizer.zero_grad()
 	
         output = model(im_o)
@@ -52,8 +67,10 @@ for epoch in range(opt.epochs):
 
         running_loss += loss.item()
         if i % 200 == 199:    # print every 200 mini-batches
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 2000))
+            print('[Epoch %d, Batch %2d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 200), end="\r")
             running_loss = 0.0
+    print('[Epoch %d, Batch %2d] loss: %.3f' %
+         (epoch + 1, i + 1, running_loss / 200), end="\r")
 
 #TODO add evaluation, model checkpointing, tensorboard
