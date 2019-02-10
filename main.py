6827f426959de99027b13c2b9638a22c8eea7808
proceduralia <pierluca.doro@gmail.com>
2019-02-10 11:40:09 +0100

    addec onditioned training

--- main.py
+++ main.py
@@ -10,21 +10,21 @@ import datetime
 import os
 import random
 from datasets import FivekDataset
-from models import CAN
+from models import CAN, ConditionalCAN
 from torch_utils import JoinedDataLoader
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--batch_size', type=int, default=8, help='input batch size')
 parser.add_argument('--epochs', type=int, default=100, help='number of epochs to train for')
-parser.add_argument('--lr', type=float, default=2e-4, help='learning rate')
+parser.add_argument('--lr', type=float, default=2e-5, help='learning rate')
 parser.add_argument('--cuda', action='store_true', help='enables cuda')
 parser.add_argument('--cuda_idx', type=int, default=1, help='cuda device id')
 parser.add_argument('--manual_seed', type=int, help='manual seed')
 parser.add_argument('--logdir', default='log', help='logdir for tensorboard')
 parser.add_argument('--run_tag', default='', help='tags for the current run')
 parser.add_argument('--checkpoint_every', default=10, help='number of epochs after which saving checkpoints')
-parser.add_argument('--checkpoint_dir', default="checkpoints", help='directory for the checkpoints')
-parser.add_argument('--model_type', default='can32', choices=['can32'], help='type of model to use')
+parser.add_argument('--checkpoint_dir', default="checkpoints", help='ibase directory for the checkpoints')
+parser.add_argument('--model_type', default='can32', choices=['can32', 'conditional_can32'], help='type of model to use')
 parser.add_argument('--data_path', default='/home/iacv3_1/fivek', help='path of the base directory of the dataset')
 opt = parser.parse_args()
 
@@ -48,6 +48,9 @@ if torch.cuda.is_available() and not opt.cuda:
 	print("You should run with CUDA.")
 device = torch.device("cuda:"+str(opt.cuda_idx) if opt.cuda else "cpu")
 
+#If the model is conditioned, the datasets must return also image features
+conditioned = opt.model_type == 'conditional_can32'
+
 landscape_transform = transforms.Compose([
                               transforms.Resize((332, 500)),
                               transforms.ToTensor(),
@@ -58,8 +61,8 @@ portrait_transform = transforms.Compose([
                               transforms.ToTensor(),
                               transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)) #normalize in [-1,1]
                              ])
-landscape_dataset = FivekDataset(opt.data_path, expert_idx=2, transform=landscape_transform, filter_ratio="landscape")
-portrait_dataset = FivekDataset(opt.data_path, expert_idx=2, transform=portrait_transform, filter_ratio="portrait")
+landscape_dataset = FivekDataset(opt.data_path, expert_idx=2, transform=landscape_transform, filter_ratio="landscape", use_features=conditioned)
+portrait_dataset = FivekDataset(opt.data_path, expert_idx=2, transform=portrait_transform, filter_ratio="portrait", use_features=conditioned)
 
 
 train_size = int(0.8 * len(landscape_dataset))
@@ -81,6 +84,10 @@ test_loader = JoinedDataLoader(test_landscape_loader, test_portrait_loader)
 
 if opt.model_type == 'can32':
   model = CAN(n_channels=32)
+if opt.model_type == 'conditional_can32':
+  #Fivek features: (subject, light, location, time)
+  nums_classes = (6, 3, 3, 4)
+  model = ConditionalCAN(nums_classes, n_channels=32)
 assert model
 
 model = model.to(device)
@@ -92,17 +99,24 @@ test_idxs = random.sample(range(len(test_landscape_dataset)), 3)
 for epoch in range(opt.epochs):
     model.train()
     cumulative_loss = 0.0
-    for i, (im_o, im_t) in enumerate(train_loader):
+    for i, vals in enumerate(train_loader):
+      optimizer.zero_grad()
+      if opt.model_type == 'conditional_can32':
+        im_o, im_t, feats = vals
         im_o, im_t = im_o.to(device), im_t.to(device)
-        optimizer.zero_grad()
-        
+        feats = tuple([feat.to(device) for feat in feats])
+        output = model(im_o, feats)
+      else:
+        im_o, im_t = vals
+        im_o, im_t = im_o.to(device), im_t.to(device)
         output = model(im_o)
-        loss = criterion(output, im_t)
-        loss.backward()
-        optimizer.step()
-        cumulative_loss += loss.item()
-        print('[Epoch %d, Batch %2d] loss: %.3f' %
-         (epoch + 1, i + 1, cumulative_loss / (i+1)), end="\r")
+
+      loss = criterion(output, im_t)
+      loss.backward()
+      optimizer.step()
+      cumulative_loss += loss.item()
+      print('[Epoch %d, Batch %2d] loss: %.3f' %
+       (epoch + 1, i + 1, cumulative_loss / (i+1)), end="\r")
     #Evaluate 
     writer.add_scalar('MSE Train', cumulative_loss / len(train_loader), epoch)
     #Checkpointing
@@ -112,8 +126,17 @@ for epoch in range(opt.epochs):
     #Model evaluation
     model.eval() 
     test_loss = []
-    for i, (im_o, im_t) in enumerate(test_loader): 
-      im_o, im_t = im_o.to(device), im_t.to(device)
+    for i, vals in enumerate(test_loader): 
+      if opt.model_type == 'conditional_can32':
+        im_o, im_t, feats = vals
+        im_o, im_t = im_o.to(device), im_t.to(device)
+        feats = tuple([feat.to(device) for feat in feats])
+        output = model(im_o, feats)
+      else:
+        im_o, im_t = vals
+        im_o, im_t = im_o.to(device), im_t.to(device)
+        output = model(im_o)
+
       with torch.no_grad():
         output = model(im_o)
         test_loss.append(criterion(output, im_t).item())
@@ -127,3 +150,6 @@ for epoch in range(opt.epochs):
       images = torch.cat((original, estimated, actual))
       grid = make_grid(images, nrow=1, normalize=True)
       writer.add_image('{}:Original|Estimated|Actual'.format(idx), grid, epoch)
+
+#Save final model
+torch.save(model.state_dict(), os.path.join(opt.checkpoint_dir, "{}_epoch{}.pt".format(opt.run_tag, opt.epochs+1)))
