76b981c5bb0934212489c0da0c8b89b6a3e8d663
proceduralia <pierluca.doro@gmail.com>
2019-02-13 15:11:40 +0100

    custom loss functions

---
+++ loss.py
@@ -0,0 +1,54 @@
+from gaussian import GaussianSmoothing
+from models import VGG
+import torch
+import torch.nn as nn
+from nima.inference.inference_model import InferenceModel
+
+class ColorContentLoss(nn.Module):
+    def __init__(self,device):
+        super(ColorContentLoss, self).__init__()
+        self.vgg = VGG(device)
+        self.vgg = self.vgg.to(device)
+        self.smoothing = GaussianSmoothing(3,10,5)
+        self.smoothing = self.smoothing.to(device)
+        
+    def __call__(self,original_img,target_img):
+        return self.color_loss(original_img,target_img) + self.content_loss(original_img,target_img) #+ self.tv_loss(original_img,target_img)
+        
+    def content_loss(self,original_img,target_img):
+        _, c1, h1, w1 = original_img.size()
+        chw1 = c1 * h1 * w1
+        vgg_original = self.vgg(original_img).detach()
+        vgg_enhanched = self.vgg(target_img)
+        content_loss = 1.0/chw1 * nn.MSELoss()(vgg_enhanched,vgg_original)
+        #print(content_loss)
+        return content_loss
+
+    def color_loss(self,original_img,target_img):
+        batch_size = original_img.size()[0]
+        original_blur = self.smoothing(original_img)
+        target_blur = self.smoothing(target_img)
+        color_loss = torch.sum(torch.pow(target_blur - original_blur,2))/(2*batch_size)
+        #print(color_loss)
+        return color_loss
+
+
+    def tv_loss(self, enhanced_img, target_img):
+        bsize, chan, height, width = target_img.size()
+        errors = []
+        dt = torch.abs(target_img[:,:,1:,:] - target_img[:,:,:-1,:])
+        de = torch.abs(enhanced_img[:,:,1:,:] - enhanced_img[:,:,:-1,:])
+        error = torch.norm(dt - de, 1)
+        return error / height
+
+class NimaLoss(nn.Module):
+    def __init__(self,device,gamma):
+        super(NimaLoss,self).__init__()
+        self.model = InferenceModel(device)
+        self.fidelity = nn.SmoothL1Loss()
+        self.fidelity = self.fidelity.to(device)
+        self.gamma = gamma
+
+    def forward(self,x,y): 
+        score = self.model.predict(x)
+        return self.fidelity(x,y) + self.gamma*(10 - score)
